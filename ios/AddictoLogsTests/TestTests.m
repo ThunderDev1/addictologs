cc70111241812690c2c23bfa6da2fe9c9c8f67dc
Hackerman <sgranger1@gmail.com>
2024-07-25 09:54:35 +0200

    Rename project

--- ios/AddictoLogsTests/TestTests.m
+++ ios/AddictoLogsTests/TestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TestTests : XCTestCase
+@interface AddictoLogsTests : XCTestCase
 
 @end
 
-@implementation TestTests
+@implementation AddictoLogsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
